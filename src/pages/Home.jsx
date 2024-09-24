513e507715bd5cc0e7db0fca1937a0f6dace6106
Anibal-quintero <amezaquintero5@gmail.com>
2024-09-24 11:13:20 -0500

    home page adapted to devices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,18 @@ import "./Home.css";
 const Home = () => {
   return (
     <section className="home">
-      <h1 className="home__title">
-        So, you want to travel to
-        <span className="home__highlight">SPACE</span>
-      </h1>
-      <p className="home__description">
-        Let’s face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we’ll give you a truly out of this world
-        experience!
-      </p>
+      <div className="home__container">
+        <h1 className="home__title">
+          So, you want to travel to
+          <span className="home__highlight">SPACE</span>
+        </h1>
+        <p className="home__description">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
       <Link to="/destination" className="home__explorer">
         Explore
       </Link>
